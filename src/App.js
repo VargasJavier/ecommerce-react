768b0eecf36efa32c05ec4fdbd40c3e0aca7f936
Javier Vargas <jabo94454@gmail.com>
2022-04-09 14:51:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1> Hola mundo! </h1> <hr />
+      <h2> ¿Quién soy ? </h2>
+      <p>
+        ¿Qué tal ? , soy Javier Alejandro Vargas Mexico y este es mi primera
+        entrega del curso de Reactjs en CODERHOUSE.
+      </p>
     </div>
   );
 }
